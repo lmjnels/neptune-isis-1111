b6175a15f2f8c6106df29308f839c7e3290dc491
Liam Nelson <liam@patterned.digital>
2022-10-25 00:24:59 +0100

    Add blank Welcome page

---
+++ resources/js/Pages/Welcome/Welcome.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Carousel from "../../Container/Carousel";
+
+export type WelcomeProps = {}
+
+export default function Welcome({}: WelcomeProps) {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    );
+}
