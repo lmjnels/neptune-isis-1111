cbf1de226676870ed8338e0aea96e43b5bf7b107
Liam Nelson <liam@patterned.digital>
2022-10-27 22:02:12 +0100

    Add Navigation to Welcome page

--- resources/js/Pages/Welcome/Welcome.tsx
+++ resources/js/Pages/Welcome/Welcome.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Carousel from "@/Container/Carousel";
+import NavigationBar from "../../Components/NavigationBar/NavigationBar";
 
 export type WelcomeProps = {}
 
 export default function Welcome({}: WelcomeProps) {
     return (
         <>
-            <h1>Welcome</h1>
+            <NavigationBar/>
             <Carousel/>
         </>
     );
